dd1cc0c54db3e62320b10b8493f3fd2d7529c85f
Tugdual de Kerviler <dekervit@gmail.com>
2020-02-21 16:27:36 +0100

    Step 3: Adjust the rating by tapping on a star

--- extra-blocks/rating-star/edit.js
+++ extra-blocks/rating-star/edit.js
@@ -1,21 +1,42 @@
 /**
  * External dependencies
  */
-import { View } from 'react-native';
+import { range } from 'lodash';
+import { TouchableWithoutFeedback, View } from 'react-native';
+
+/**
+ * WordPress dependencies
+ */
+import { useState } from '@wordpress/element';
 
 /**
  * Internal dependencies
  */
 import { StarIcon } from './icon';
 
+const TouchableStarIcon = ( { filled, onPress } ) => {
+	return (
+		<TouchableWithoutFeedback onPress={ onPress }>
+			<View>
+				<StarIcon filled={ filled } />
+			</View>
+		</TouchableWithoutFeedback>
+	);
+};
+
 export default () => {
+	const [ rating, setRating ] = useState( 3 );
 	return (
 		<View style={ { flex: 1, flexDirection: 'row' } }>
-			<StarIcon filled={ true } />
-			<StarIcon filled={ true } />
-			<StarIcon filled={ true } />
-			<StarIcon />
-			<StarIcon />
+			{
+				range( 1, 6 ).map( ( starNumber ) => (
+					<TouchableStarIcon
+						key={ starNumber }
+						filled={ starNumber <= rating }
+						onPress={ () => setRating( starNumber ) }
+					/>
+				) )
+			}
 		</View>
 	);
 };
